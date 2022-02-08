d58beafca639563659325da4a91932aad2181886
Gregory Mierzwinski <gmierz1+1@live.ca>
2022-02-07 21:01:29 -0500

    Add lab 04 instructions.

--- bishops/cs321/populate_labs.js
+++ bishops/cs321/populate_labs.js
@@ -39,7 +39,48 @@ const LABS = [
 		show: true
 	},
 	{
-		title: "Lab 04 - TBD",
+		title: "Lab 04 - Interfaces, and Inheritance",
+		date: "February 8, 2022",
+		subtitle: "",
+		summary: `
+            <a href="resources/CS321_Lab_04.pdf" style="color: blue" target="_blank">
+                Lab Instructions
+            </a> <br>
+		`,
+		show: true
+	},
+	{
+		title: "Lab 05 - TBD",
+		date: "February 8, 2022",
+		subtitle: "",
+		summary: `
+            <br>
+            TBD
+		`,
+		show: true
+	},
+	{
+		title: "Lab 06 - TBD",
+		date: "February 8, 2022",
+		subtitle: "",
+		summary: `
+            <br>
+            TBD
+		`,
+		show: true
+	},
+	{
+		title: "Lab 07 - TBD",
+		date: "February 8, 2022",
+		subtitle: "",
+		summary: `
+            <br>
+            TBD
+		`,
+		show: true
+	},
+	{
+		title: "Lab 08 - TBD",
 		date: "February 8, 2022",
 		subtitle: "",
 		summary: `
