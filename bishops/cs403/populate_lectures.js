df37d9c69299a6c9baa7ac7c266e180f270e408c
Gregory Mierzwinski <gmierzwi@ubishops.ca>
2022-10-10 19:15:26 -0400

    Add prolog slides.

--- bishops/cs403/populate_lectures.js
+++ bishops/cs403/populate_lectures.js
@@ -20,14 +20,14 @@ const LECTURES = [
 		show: true
 	},
 	{
-		title: "Lecture 02 - Haskell (1/4)",
+		title: "Lecture 02 to 06 - Haskell",
 		date: "September 13th, 2022",
 		subtitle: "",
 		summary: `
             <a href="resources/CS403_Lecture_02.pdf" id="a_blue_link" target="_blank">
-                Haskell
-            </a> (1-20) <br>
-            <a href="resources.html#functional-resources" id="a_blue_link" target="_blank">
+                Haskell Slides
+            </a> <br>
+            <a href="resources.html#functional-resources" id="a_blue_link">
                 Functional Programming Resources
             </a>
             <br> <br>
@@ -35,35 +35,17 @@ const LECTURES = [
 		show: true
 	},
 	{
-		title: "Lecture 03 - Haskell (2/4)",
-		date: "September 15th, 2022",
+		title: "Lecture 07 to 10 - Prolog",
+		date: "October 11th, 2022",
 		subtitle: "",
 		summary: `
-            <a href="resources/CS403_Lecture_02.pdf" id="a_blue_link" target="_blank">
-                Haskell
-            </a> (20-40) <br> <br>
-		`,
-		show: true
-	},
-	{
-		title: "Lecture 04 - Haskell (3/4)",
-		date: "September 20th, 2022",
-		subtitle: "",
-		summary: `
-            <a href="resources/CS403_Lecture_02.pdf" id="a_blue_link" target="_blank">
-                Haskell
-            </a> (40-65) <br> <br>
-		`,
-		show: true
-	},
-	{
-		title: "Lecture 05 - Haskell (4/4)",
-		date: "September 22nd, 2022",
-		subtitle: "",
-		summary: `
-            <a href="resources/CS403_Lecture_02.pdf" id="a_blue_link" target="_blank">
-                Haskell
-            </a> (final slides + review) <br> <br>
+            <a href="resources/CS403_Lecture_03.pdf" id="a_blue_link" target="_blank">
+                Prolog Slides
+            </a> <br>
+            <a href="resources.html#logic-resources" id="a_blue_link">
+                Logic Programming Resources
+            </a>
+            <br> <br>
 		`,
 		show: true
 	},
