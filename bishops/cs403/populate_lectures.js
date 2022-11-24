1159645c36072b1742feb2e6ecf484ac43eef7de
Gregory Mierzwinski <gmierzwi@ubishops.ca>
2022-11-24 13:15:02 -0500

    Fix up lecture naming, and links.

--- bishops/cs403/populate_lectures.js
+++ bishops/cs403/populate_lectures.js
@@ -83,32 +83,29 @@ const LECTURES = [
 		show: true
 	},
 	{
-		title: "Lecture Set 06 - Semantic Analysis",
+		title: "Lecture Set 06 - Types, & Classes",
 		date: "",
 		subtitle: "",
 		summary: `
-            <a href="resources/CS403_Lecture_05.pdf" id="a_blue_link" target="_blank">
-                Semantic Analysis Slides
-            </a> <br>
-            <a href="resources.html#semantic-resources" id="a_blue_link">
-                Semantics Resources
+            <a href="resources/CS403_Lecture_06.pdf" id="a_blue_link" target="_blank">
+                Types, & Classes Slides
             </a> <br> <br>
 		`,
 		show: true
 	},
 	{
-		title: "Lecture Set 07 - Semantic Analysis",
+		title: "Lecture Set 07 - Subprograms",
 		date: "",
 		subtitle: "",
 		summary: `
             <a href="resources/CS403_Lecture_07.pdf" id="a_blue_link" target="_blank">
-                Semantic Analysis Slides
+                Subprograms Slides
             </a> <br> <br>
 		`,
 		show: true
 	},
 	{
-		title: "Lecture Set 08 - Semantic Analysis",
+		title: "Lecture Set 08 - Pointers",
 		date: "",
 		subtitle: "",
 		summary: `
