c84278204f2b564631f8b721764b1938357d388e
Gregory Mierzwinski <gmierzwi@ubishops.ca>
2022-12-19 13:57:56 -0500

    Update deadline.

--- bishops/cs403/populate_final_project.js
+++ bishops/cs403/populate_final_project.js
@@ -6,7 +6,7 @@ a JSON defined in this file.
 
 const PROJECT_SECTIONS = [
 	{
-		title: `<div style="color:red">Due: December 28th (midnight)`,
+		title: `<div style="color:red">Due: December 30th (midnight)`,
 		date: "",
 		subtitle: "",
 		summary: `
